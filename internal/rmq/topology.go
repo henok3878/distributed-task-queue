c9618688343e6ff6eee556fe0551c70b44429733
henok3878 <81500872+henok3878@users.noreply.github.com>
2025-09-01 19:01:38 -0400

    feat(rmq): add derived topology from RMQ_NAMESPACE & QUEUES

---
+++ internal/rmq/topology.go
@@ -0,0 +1,53 @@
+package rmq
+
+import (
+	"strings"
+
+	"github.com/henok3878/distributed-task-queue/internal/config"
+)
+
+type Topology struct {
+	Namespace    string   // ex: "tasks"
+	MainExchange string   // "<ns>.direct"
+	MainKind     string   // "direct"
+	DLXExchange  string   // "<ns>.dlx"
+	DLXKind      string   // "direct"
+	QueuePrefix  string   // "<ns>"
+	DLQName      string   // "<ns>.dlq"
+	RoutingKeys  []string // e.g. ["default","high"]
+}
+
+func Load() (Topology, error) {
+	ns, err := config.GetFromEnv("RMQ_NAMESPACE")
+	if err != nil {
+		return Topology{}, err
+	}
+
+	queuesCSV, err := config.GetFromEnv("QUEUES") // ex: "default,high"
+	if err != nil {
+		return Topology{}, err
+	}
+
+	var rks []string
+	for _, s := range strings.Split(queuesCSV, ",") {
+		if v := strings.TrimSpace(s); v != "" {
+			rks = append(rks, v)
+		}
+	}
+
+	t := Topology{
+		Namespace:    ns,
+		MainExchange: ns + ".direct",
+		MainKind:     "direct",
+		DLXExchange:  ns + ".dlx",
+		DLXKind:      "direct",
+		QueuePrefix:  ns,
+		DLQName:      ns + ".dlq",
+		RoutingKeys:  rks,
+	}
+	return t, nil
+}
+
+func (t Topology) FullQueueName(rk string) string {
+	return t.QueuePrefix + "." + rk
+}
